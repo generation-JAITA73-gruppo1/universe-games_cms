2ec8370462f82b6662d8a8683130a2ca1e85d391
Alessandro Tecchio <tecchio.alessandro.96@gmail.com>
2023-02-08 18:34:33 +0100

    feat: intial routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { InserimentoComponent } from './pages/inserimento/inserimento.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomepageComponent, InserimentoComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, SharedModule],
 })
-export class AppModule { }
+export class AppModule {}
