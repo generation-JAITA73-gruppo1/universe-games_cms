08d44bf72690936b13d09bdfe9b3965b7eca26c4
3ug3n <epopescuu@yahoo.it>
2023-02-08 16:47:35 +0100

    news chiamate API

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { News } from '../model/news';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private apiUrl = 'https://project-works-rest-api.onrender.com/api/v1/GROUP-I/news'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getNews() {
+    return this.http.get<News[]>(this.apiUrl)
+  }
+  
+  postNews(newNews: Omit<News, 'id'>) {
+    return this.http.post(this.apiUrl, newNews)
+  }
+
+  //manca il metodo put 
+
+  deleteNews(id: number) {
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
 }
