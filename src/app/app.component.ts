f11855ff8b0feef413a4f87ef35cff02e4d51526
Alessandro Tecchio <tecchio.alessandro.96@gmail.com>
2023-02-08 17:21:26 +0100

    feat: add recensione api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recensione } from './model/recensione';
+import { RecensioneService } from './service/recensione.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'universe-games_csm';
+  lista$!: Observable<Recensione[]>;
+
+  constructor(private service: RecensioneService) {}
+
+  ngOnInit(): void {
+    this.lista$ = this.service.getRecensioni();
+  }
 }
