3a44e1c408c1864daa83d33e4569b4e4854b3c1e
Ettore Menorello <ettore.menoello@gmail.com>
2023-02-09 16:48:16 +0100

    feat: lista-recensioni

--- src/app/lista-recensioni/lista-recensioni.component.ts
+++ src/app/lista-recensioni/lista-recensioni.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Recensione } from '../model/recensione';
+import { RecensioneService } from '../service/recensione.service';
 
 @Component({
   selector: 'app-lista-recensioni',
   templateUrl: './lista-recensioni.component.html',
   styleUrls: ['./lista-recensioni.component.css']
 })
-export class ListaRecensioniComponent {
+export class ListaRecensioniComponent implements OnInit{
+  recensioni!: Recensione[];
+  recensioni$!: Observable<Recensione[]>;
+  recensioniSubscription!: Subscription;
 
+  constructor(private recensioneService: RecensioneService) {}
+
+  ngOnInit(): void {
+    this.recensioni$ = this.recensioneService.getRecensioni();
+  }
 }
--- src/app/service/recensione.service.ts
+++ src/app/service/recensione.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Recensione } from '../model/recensione';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class RecensioneService {
   private apiUrl =
     'https://project-works-rest-api.onrender.com/api/v1/GROUP-I/review';
 
   constructor(private http: HttpClient) {}
+
+  private recensioneSubject = new Subject<Recensione[]>();
+
+  private recensione: Recensione[] = [];
+
+  recenzione$ = this.recensioneSubject.asObservable();
 
   getRecensioni(): Observable<Recensione[]> {
     return this.http.get<Recensione[]>(this.apiUrl);
