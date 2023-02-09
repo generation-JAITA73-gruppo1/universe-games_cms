247afb969069d14c88a6c331dcd755935ef05bd9
3ug3n <epopescuu@yahoo.it>
2023-02-09 15:43:15 +0100

    lista-news

--- src/app/lista-news/lista-news.component.ts
+++ src/app/lista-news/lista-news.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { News } from '../model/news';
+import { NewsService } from '../service/news.service';
 
 @Component({
   selector: 'app-lista-news',
   templateUrl: './lista-news.component.html',
   styleUrls: ['./lista-news.component.css']
 })
-export class ListaNewsComponent {
+export class ListaNewsComponent implements OnInit{
+  news!: News[];
+  news$!: Observable<News[]>;
+  newsSubscription!: Subscription;
 
+  constructor(private videogiocoService: NewsService) {}
+
+  ngOnInit(): void {
+    this.news$ = this.videogiocoService.getNews();
+  }
 }
